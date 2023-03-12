88927ac2d98977c72f026ea4c42675373eca5044
Andrei <andrei.borod1n@mail.ru>
2023-03-12 20:26:16 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+int n = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[n];
+for(int i = 0; i < n; i++) {
+    arr[i] = Convert.ToInt32(new Random().Next(-100, 100));
+    Console.WriteLine($"{arr[i]} ");
+}
+int value = 0;
+for(int i = 0; i < n; i++) {
+    if (arr[i] > 0) value++;
+}
+Console.Write($"Пользователь ввёл {value} чисел, которые больше нуля");
